e5b518600d1747441e578b738951dc1864fcc78a
Marathon <3045130265@qq.com>
2025-12-22 16:15:05 +0800

    排序操作声明

---
+++ 25_12_22_Sort/Sort.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <stdbool.h>
+#include <time.h>
+
+void InsertSort(int* a, int n);
+void ShellSort(int* a, int n);
