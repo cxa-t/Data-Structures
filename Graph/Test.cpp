996cb114aa8f943c1b0cf035d81ce9db671f45b7
cxa-t <134744746+cxa-t@users.noreply.github.com>
2026-05-29 14:57:25 +0800

    测试代码

---
+++ Graph/Test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+#include"Unionfindset.h"
+#include"Graph.h"
+
+
+int main()
+{
+	//Matrix::TestGraph1();
+	//Matrix::TestBDFS();
+	//Matrix::TestCircleBDFS();
+	Link_table::TestGraph1();
+
+	return 0;
+}
